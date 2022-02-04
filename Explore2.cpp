62fd3c9c8a636f21e833cb6b0cb23100dc7cd911
SASHAVODA2 <>
2022-02-04 17:10:58 +0000

    Explore2 done

--- Explore2.cpp
+++ Explore2.cpp
@@ -1,4 +1,3 @@
-
 #include <iostream>
 using namespace std;
 struct Point {
@@ -8,7 +7,6 @@ struct Point {
 };
 Point::Point() {
 	// Normally, you do not print in a constructor
-	cout << "In the constructor" << endl;
 	x = 0;
 	y = 0;
 }
